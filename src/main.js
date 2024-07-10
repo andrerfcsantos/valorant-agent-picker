ebc276e00282d4003e2637db720ad8b87f501523
André Santos <andrerfcsantos@gmail.com>
2024-07-10 07:25:28 +0100

    Remove vercel analytics

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import store from "./store";
 import VueRouter from "vue-router";
 import { routes } from "./router/routes";
 import { NavbarPlugin } from "bootstrap-vue";
-import { inject } from "@vercel/analytics";
-
-inject();
 
 Vue.config.productionTip = false;
 
